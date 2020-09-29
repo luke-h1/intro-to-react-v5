f271124f271d49783ddf93942ddec61061359692
luke-h1 <luke.howsam@yahoo.com>
2020-09-29 18:23:25 +0100

    setting state with hooks

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchParams = () => {
-  const location = 'Seattle, WA';
+  const [location, setLocation] = useState('Seattle, WA');
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="location"></input>
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={(e) => setLocation(e.target.value)}
+          ></input>
         </label>
         <button>Submit</button>
       </form>
