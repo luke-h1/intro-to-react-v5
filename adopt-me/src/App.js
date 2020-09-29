977c36e12cac8dfa76f84ee640247effe856e44a
luke-h1 <luke.howsam@yahoo.com>
2020-09-29 18:02:43 +0100

    turn into jsx

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import  Pet  from './Pet';
+import Pet from './Pet';
 const App = () => {
-  return React.createElement('div', { id: 'something important' }, [
-    React.createElement('h1', {}, 'Adopt Me'),
-    React.createElement(Pet, {
-      name: 'luna',
-      animal: 'dog',
-      breed: 'havanese',
-    }),
-    React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'dog',
-      breed: 'Cockatiel',
-    }),
-    React.createElement(Pet, {
-      name: 'doink',
-      animal: 'cat',
-      breed: 'Mixed',
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
